7436851008c4cea133dec822646b669b9c111989
Toni Pizà <tpiza@habitissimo.com>
2021-03-05 22:33:00 +0100

    Form fields

--- mldeploy/forms.py
+++ mldeploy/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
+from wtforms import SelectField, StringField, TextAreaField
+from wtforms.validators import DataRequired, regexp
 
 
 class AddModelForm(Form):
-    name = StringField('name', validators=[DataRequired()])
-    description = StringField('description', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    training_library = SelectField('Training library', choices=[('scikit-learn', 'scikit-learn'), ('tensorflow', 'tensorflow')])
+    serialized_model = FileField('Serialized model file', validators=[DataRequired(), regexp(r'^[^/\\]\.joblib$')])
